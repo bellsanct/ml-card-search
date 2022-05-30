9d4f15a67c44450e61d978b573625f8a88d2434f
bellsanct <bellsanct@gmail.com>
2022-05-30 11:34:48 +0900

    [add]検索ファイル仮追加

--- search.php
+++ search.php
@@ -1,15 +1,18 @@
 <?php
-$search_target = './outputs/037nao0063_1_b.png_.txt';
+$search_target = './outputs/037nao0074_0_b.txt';
 $search_file = fopen($search_target, 'r'); 
+$array_results = array();
 
 // whileで行末までループ処理
 while (!feof($search_file)) {
   // fgetsでファイルを読み込み、変数に格納
   $search_bits = fgets($search_file);
-  echo $search_bits . "\n";
-  $pop1 = gmp_init($search_bits, 2);
-  echo gmp_popcount($pop1) . "\n";
+  //echo $search_bits . "\n";
+  //$pop1 = gmp_init($search_bits, 2);
+  //echo gmp_popcount($pop1) . "\n";
 }
+
+$array_search_bits = str_split($search_bits);
 
 //$files = glob("./images/*.png");
 
@@ -17,7 +20,22 @@ $files = glob("./outputs/*.txt");
 for($count = 0; $count < count($files); $count++) {
   $target_file = $files[$count];
   $target_bits = file_get_contents($target_file);
-  //echo $target_bits . "\n";
-}
+  $array_target_bits = str_split($target_bits);
 
+  $parity = 0;
+
+  for($strcount = 0 ; $strcount < 20 ; $strcount++) {
+    //echo $search_bits[$strcount] . " " . $target_bits[$strcount] . "\n"; 
+    if($search_bits[$strcount] xor $target_bits[$strcount]) {
+      //echo $search_bits[$strcount] . " XOR " . $target_bits[$strcount] . " is TRUE" . "\n";
+      $parity++;
+    } else {
+      //echo $search_bits[$strcount] . " XOR " . $target_bits[$strcount] . " is FALSE" . "\n";
+    }
+  }
+  if($parity < 4) {
+      //$array_results += array("image_id" => basename($target_file, ".txt"), "parity" => $parity);
+      echo basename($search_target, ".txt") . " and " . basename($target_file, ".txt") . " parity is " . $parity . "\n";
+  }
+}
 ?>
